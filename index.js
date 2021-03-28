b7316038970872b751a6818c9ba255a8b877b3bd
Colin Tseng <klt.aus@yahoo.com>
2021-03-28 19:54:35 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+let names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(arrayNames, event) {
+
+    let thankYouMessages = [];
+    for (let i = 0; i < arrayNames.length; i++ ) {
+    
+    thankYouMessages.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages
+}
+
+writeCards(names, "surprise");
+
+
+
+function countDown(number) {
+    while (number >= 0) {
+
+        console.log(number--);
+        // number--
+      }
+      return "All done";
+}
+countDown(10);
